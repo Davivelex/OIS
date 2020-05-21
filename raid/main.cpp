629b034189e58d4a2c37585d67d140becd311df9
davide.leva33@gmail.com <davide.leva33@gmail.com>
2020-05-21 18:15:06 +0200

    Added raid and PDF docs

---
+++ raid/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, P, C = 0, h;
+vector<int> H;
+
+int main(int argc, char** argv) {
+	freopen("input.txt", "r", stdin);
+	
+	cin >> N >> P;
+	
+	for(int i=0; i<N; i++) {
+		cin >> h;
+		H.push_back(h);
+	}
+	
+	for(int p=0; p<P; p++) {
+		for(int i=0; i<N; i++) {
+			if(i == 0 && H[i] != 0 && H[i] > H[i+1]) {
+				H[0] = 0;
+				C++;
+				i++;
+			} else if(H[i] != 0 && H[i] > H[i-1] && H[i] > H[i+1]) {
+				H[i] = 0;	
+				C++;
+				i++;
+			} else if(i == N - 1 && H[i] != 0 && H[i] > H[i-1]) {
+				H[N-1] = 0;
+				C++;
+				i++;
+			}
+		}
+	}
+	
+	cout << C;
+	return 0;
+}
